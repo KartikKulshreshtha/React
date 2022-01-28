d5488c425cbb2d6877876985c7cfc63952e995d2
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2022-01-28 15:50:43 +0530

    Final Commit of text utils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,6 +26,12 @@ export default function TextForm(props) {
         setText(full)
         props.showAlert("First word has been capitalized", 'success')
     }
+    const handleCopy = ()=>{
+        let text = document.getElementById('exampleFormControlTextarea1').innerHTML
+        
+        navigator.clipboard.writeText(text)
+        // props.showAlert("Copied to ClipBoard", 'success')
+    }
     return (
         <>
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
@@ -35,13 +41,13 @@ export default function TextForm(props) {
                 </div>
                 <button disabled={text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleClear}>Clear all the text</button>
+                <button id='BOX' disabled={text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleCopy}>Copy all the text</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleCapatalize}>Capatalize your first word</button>
             </div>
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>Your text summary</h1>
-                <p >{((text.trim().split(" ")).filter(function (element) {
-                    return element !== "";
-                })).length} words and {text.length} characters </p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.
+                    length!==0}).length} words and {text.length} characters</p>
                 <p>{text.length === 0? '0':0.008 * text.split(" ").length} Minutes to read</p>
                 <h4>Preview</h4>
                 <p>{text.length > 0 ? text : "Enter something in the textarea above to preview it here"}</p>
